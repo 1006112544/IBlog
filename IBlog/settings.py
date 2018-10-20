55c260fe9ca6914b5556ec10ac6570f98851645d
dabao <1006112544@qq.com>
2018-10-20 10:42:23 +0800

    mysql数据库

--- IBlog/settings.py
+++ IBlog/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'IBlog.wsgi.application'
 # 数据库
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',   # 数据库引擎
-        'NAME': 'iblogdata',       # 你要存储数据的库名，事先要创建之
-        'USER': 'root',         # 数据库用户名
-        'PASSWORD': '545764311',     # 密码
-        'HOST': 'localhost',    # 主机
-        'PORT': '3306',         # 数据库使用的端口
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.mysql',   # 数据库引擎
+        # 'NAME': 'iblogdata',       # 你要存储数据的库名，事先要创建之
+        # 'USER': 'root',         # 数据库用户名
+        # 'PASSWORD': '545764311',     # 密码
+        # 'HOST': 'localhost',    # 主机
+        # 'PORT': '3306',         # 数据库使用的端口
     }
 }
 
